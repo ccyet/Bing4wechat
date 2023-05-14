0e8a0daf248ce31cb8b073230c3bc99eb70a1612
ccyet <cccj159@gmail.com>
2023-05-14 14:05:17 +0800

    Update main.py

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ def handler_group_msg(msg):
 
 class weChat():
     def __init__(self):
-        self.bot = asyncio.run(Chatbot.create(cookie_path='./cookies.json',proxy="http://0.0.0.0:$PORT"))
+        self.bot = asyncio.run(Chatbot.create(cookie_path='./cookies.json',proxy="http:///0.0.0.0:7660"))
 
     def startup(self):
         # login by scan QRCode
